8de503ea16e9a10767ba4a110d09c1b1e9c4db24
Johan Nordlinder <johan.nordlinder@lm.se>
2023-12-14 14:05:52 +0100

    Initial commit

--- src/main/kotlin/minimal/reproducible/example/demo/TestDataSetup.kt
+++ src/main/kotlin/minimal/reproducible/example/demo/TestDataSetup.kt
@@ -1,19 +1,23 @@
 package minimal.reproducible.example.demo
 
+import jakarta.persistence.EntityManager
 import org.slf4j.LoggerFactory
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
-import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.Duration
 import java.time.Instant
 import kotlin.random.Random
 
 @Component
-class TestDataSetup(val repo: TestdataEntityRepository) {
+class TestDataSetup(val repo: TestdataEntityRepository, val em: EntityManager) {
 
     private val log = LoggerFactory.getLogger(this.javaClass)
+
+    val STRING_LENGTH = 100
+    val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+    fun randomString() = (1..STRING_LENGTH)
+        .map { Random.nextInt(0, charPool.size).let { charPool[it] } }
+        .joinToString("")
 
     fun createTestDataInDatabase(): Boolean {
         val rowsToCreate = 10000
@@ -21,13 +25,14 @@ class TestDataSetup(val repo: TestdataEntityRepository) {
         log.info("Starting to create $rowsToCreate rows of test data in database...")
 
         for (i in 1..rowsToCreate) {
-            repo.save(TestdataEntity(null, Random.nextInt().toString()))
+            repo.save(TestdataEntity(null, randomString()))
             if (i % 100 == 0) {
                 log.info(
                     "Finished creating $i rows, total elapsed time: ${
                         Duration.between(start, Instant.now()).toSeconds()
                     }s"
                 )
+                em.clear()
             }
         }
 
